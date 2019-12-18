93617dbdb9dd64fb065f7437937f96d08d250ef5
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-18 23:01:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,3 @@
-#
-# def speak_to_grandma(string)
-#   if string == "I LOVE YOU GRANDMA!"
-#     return "I LOVE YOU TOO PUMPKIN!"
-#   elsif string != string.upcase
-#     return "HUH?! SPEAK UP, SONNY!"
-#   elsif string == string.upcase
-#     return "NO, NOT SINCE 1938!"
-#   end
-# end
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
@@ -18,4 +8,3 @@ def speak_to_grandma(string)
     return "NO, NOT SINCE 1938!"
   end
 end
-speak_to_grandma ("hello")
