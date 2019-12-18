ee7243be35d604e3b6670c49e85c4378d5c6580c
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-18 22:32:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,20 @@
 # Write a speak_to_grandma method.
 
+name = gets.chomp
+
+if name == PUMPKIN
+  puts "I LOVE YOU GRANDMA!"
+elsif "I LOVE YOU PUMPKIN!"
+  puts "I LOVE YOU TOO GRANDMA!'"
+elsif condition
+end
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
